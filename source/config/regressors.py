66f3c1db13d163d16e4c550991d115c612aa564f
RolandColored <klausaufsendung@googlemail.com>
2017-01-29 22:15:15 +0100

    new regressors confoguration

--- source/config/regressors.py
+++ source/config/regressors.py
@@ -1,36 +1,50 @@
-from sklearn.ensemble import AdaBoostRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.kernel_ridge import KernelRidge
 from sklearn.linear_model import BayesianRidge
 from sklearn.linear_model import MultiTaskElasticNet
 from sklearn.linear_model import MultiTaskLasso
+from sklearn.linear_model import Ridge
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import NuSVR
+from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 
 
 regressor_list = [
     DecisionTreeRegressor(),
     RandomForestRegressor(),
+    ExtraTreesRegressor(),
+    MultiOutputRegressor(GradientBoostingRegressor()),
     KNeighborsRegressor(),
     MultiTaskLasso(),
     MultiTaskElasticNet(),
-    MultiOutputRegressor(BayesianRidge()),
-    MultiOutputRegressor(GradientBoostingRegressor()),
+    MultiOutputRegressor(SVR(kernel='linear')),
+    MultiOutputRegressor(SVR(kernel='rbf')),
+    MultiOutputRegressor(SVR(kernel='poly')),
+    MultiOutputRegressor(SVR(kernel='sigmoid')),
+    MultiOutputRegressor(NuSVR(kernel='linear')),
     MultiOutputRegressor(NuSVR(kernel='rbf')),
     MultiOutputRegressor(NuSVR(kernel='poly')),
     MultiOutputRegressor(NuSVR(kernel='sigmoid')),
-    ExtraTreesRegressor(),
-    MultiOutputRegressor(AdaBoostRegressor()),
+    Ridge(),
+    KernelRidge(kernel='linear'),
+    KernelRidge(kernel='rbf'),
+    KernelRidge(kernel='poly'),
+    KernelRidge(kernel='sigmoid'),
+    MultiOutputRegressor(BayesianRidge()),
 ]
+
 
 # helper function
 def get_regressor_name(regressor_obj):
-    if isinstance(regressor_obj, MultiOutputRegressor):
+    if isinstance(regressor_obj, KernelRidge):
+        regressor_name = regressor_obj.__class__.__name__ + regressor_obj.kernel
+    elif isinstance(regressor_obj, MultiOutputRegressor):
         regressor_name = regressor_obj.estimator.__class__.__name__
-        if isinstance(regressor_obj.estimator, NuSVR):
+        if isinstance(regressor_obj.estimator, NuSVR) or isinstance(regressor_obj.estimator, SVR):
             regressor_name += regressor_obj.estimator.kernel
     else:
         regressor_name = regressor_obj.__class__.__name__
