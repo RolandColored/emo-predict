756aec549a76bc57776963bc83ef21671f6d38c5
RolandColored <roland.kahlert@me.com>
2017-05-18 18:07:34 +0200

    bow combinations jobs/plots

--- source/generate_jobs.py
+++ source/generate_jobs.py
@@ -1,16 +1,19 @@
 from config.pipelineconfig import PipelineConfig
 
-print('#!/bin/sh')
 
-
-datasets = ['foxnews+nytimes+theguardian', 'bild+spiegelonline+ihre.sz']
+datasets = ['foxnews+nytimes+theguardian']#, 'bild+spiegelonline+ihre.sz']
 features = [func for func in dir(PipelineConfig) if callable(getattr(PipelineConfig, func))
             and 'tfidf' not in func
             and not func.startswith('_')]
-regressors = ['Baseline', 'Ridge']
+regressors = ['Ridge']
 
+i = 0
 
 for dataset in datasets:
     for feature in features:
         for reg_name in regressors:
-            print('qsub -v datasource=' + dataset + ',feature=' + feature + ',regressor=' + reg_name + ' emopredict.job')
+            print("job[" + str(i) + "]='python3 /home/rokah100/emo-predict/source/emopredict.py " + dataset + " " + feature + " " + reg_name + " /home/rokah100/results'")
+            i += 1
+
+print('run=${job[$PBS_ARRAY_INDEX]}')
+print('eval $run')
--- source/plots/bow_combinations.py
+++ source/plots/bow_combinations.py
@@ -18,7 +18,8 @@ df = df.sort_values('error')
 df.loc[df['regressor_name'] == 'Baseline', 'feature_generator'] = 'Baseline'
 df['feature_generator'] = df['feature_generator'].str.replace('text', '').str.replace('_', ' ').str.title()
 colors = ['C1'] * (len(df) - 1) + ['C0']
-df.plot(kind='bar', y='error', x='feature_generator', legend=False, color=colors, ylim=(0.09, 0.12))
+plot = df.plot(kind='bar', y='error', x='feature_generator', legend=False, color=colors, ylim=(0.09, 0.12))
+plot.tick_params(axis='both', which='major', labelsize=7)
 
 # labels
 plt.ylabel('RMSE')
