989ca8414ce23ad398744b0e067cf75aba8af1ef
RolandColored <klausaufsendung@googlemail.com>
2017-05-24 00:22:36 +0200

    integrated german lemmatizer

--- source/features/lemmatizer.py
+++ source/features/lemmatizer.py
@@ -1,11 +1,17 @@
+import os
+
 import spacy
+from iwnlp.iwnlp_wrapper import IWNLPWrapper
 from sklearn.base import BaseEstimator
 
 
 class Lemmatizer(BaseEstimator):
 
     def __init__(self, lang):
+        self.lang = lang
         self.nlp = nlp = spacy.load(lang)
+        current_dir = os.path.dirname(__file__)
+        self.iwnlp = IWNLPWrapper(lemmatizer_path=current_dir + '/../resources/IWNLP.Lemmatizer_20170501.json')
 
     def fit(self, X, y=None):
         return self
@@ -14,6 +20,12 @@ class Lemmatizer(BaseEstimator):
         ret_list = []
         for row in X:
             doc = self.nlp(row)
-            new_row = ' '.join([word.lemma_ for word in doc])
+
+            # workaround until german lemmatizer is integrated in spacy
+            if self.lang == 'de':
+                new_row = ' '.join([self.iwnlp.lemmatize(str(word), word.pos_) for word in doc])
+            else:
+                new_row = ' '.join([word.lemma_ for word in doc])
             ret_list.append(new_row)
+            print(new_row)
         return ret_list
